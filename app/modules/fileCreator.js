9b0c04e0b87182723509d087ceb2050565a57a57
narugit <narusens@gmail.com>
2019-04-14 16:08:55 +0900

    プロジェクト保存（プロジェクト名固定）

---
+++ app/modules/fileCreator.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let remote = require('electron').remote;
+const { dialog } = require('electron');
+let fs = require('fs');
+
+const FileCreator = class {
+    constructor(dirName) {
+        this.dirName = dirName;
+    }
+
+    saveProject() {
+        let projectName = 'ProjectHoge';
+        let content = "Some text to save into the file";
+
+        let pdir = this.dirName + '/' + projectName;
+
+        let fnameTails = ['memo', 'note', 'todo'];
+
+        fs.mkdir(pdir);
+
+        fnameTails.forEach(function (fnameTailsElment) {
+            fs.writeFile(pdir + '/' + projectName + '_' + fnameTailsElment + '.md', content, (err) => {
+                if (err) {
+                    dialog.showErrorBox("An error ocurred creating the file", err.message);
+                }
+            });
+        });
+    }
+};
+module.exports = FileCreator;
