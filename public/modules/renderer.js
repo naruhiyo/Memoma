1d53aaf2b508c414c2d5936554852889500612d0
tatsuya_oyanagi <17nm704r@vc.ibaraki.ac.jp>
2019-04-14 16:48:00 +0900

    Moved the renderer file to separate the logics.

--- public/modules/renderer.js
+++ public/modules/renderer.js
@@ -1,3 +1,10 @@
+/**
+ * Control the markdown editor.
+ *
+ * === Functions ===
+ * - Requesting any operation to `mdEditor.js`.
+ * - Switch each text form.
+ */
 const { ipcRenderer } = require('electron');
 const mdEditor = require('./app/modules/mdEditor');
 const editor = new mdEditor(document);
