34639a5722761f840b1d424a36928c4e82736b84
Tatsuya Oyanagi <sb.gm.0823@gmail.com>
2019-04-29 11:20:39 +0900

    Add change text field mode.

--- app/modules/mdEditor.js
+++ app/modules/mdEditor.js
@@ -11,35 +11,63 @@ const marked = require('marked');
 const MdEditor = function (document) {
 
     // dom elements
-    this.markdown = null;
-    this.preview = null;
+    this.markdowns = null;
+    this.previews = null;
+
+    this.activeMarkdown = null;
+    this.activePreview = null;
+
+    this.maxItemSize = 0;
+    this.currentNodeIndex = 0;
+
     this.document = document;
 
     // marked options
-    this.markedOptions = {}
+    this.markedOptions = {};
 
     this.init = () => {
-        // dynamic doms
-        this.markdown = this.document.querySelector('.markdown-text.active');
-        this.preview = this.document.querySelector('.markdown-preview.active');
+        // get All fields
+        this.markdowns = this.document.querySelectorAll('.markdown-text');
+        this.previews = this.document.querySelectorAll('.markdown-preview');
+
+        // get Active field
+        this.activeMarkdown = this.document.querySelector('.markdown-text.active');
+        this.activePreview = this.document.querySelector('.markdown-preview.active');
+
+        this.maxItemSize = this.markdowns.length;
     };
 
     //
     this.convert = () => {
-        this.init();
-
-        const text = this.markdown.value;
+        const text = this.activeMarkdown.value;
 
         // translated!
-        this.preview.innerHTML = marked(text, this.markedOptions);
+        this.activePreview.innerHTML = marked(text, this.markedOptions);
     };
 
     this.toggle = () => {
         this.convert();
 
-        this.markdown.classList.toggle('d-hidden');
-        this.preview.classList.toggle('d-hidden');
-    }
+        this.activeMarkdown.classList.toggle('d-hidden');
+        this.activePreview.classList.toggle('d-hidden');
+    };
+
+    this.change = () => {
+        const index = this.activeMarkdown.dataset.index;
+
+        this.currentNodeIndex = (index < this.maxItemSize) ? index : 0;
+
+        // remove active class from current target dom.
+        this.activeMarkdown.classList.remove('active');
+        this.activePreview.classList.remove('active');
+
+        // add active class to new target dom.
+        this.activeMarkdown = this.markdowns[this.currentNodeIndex];
+        this.activePreview = this.previews[this.currentNodeIndex];
+
+        this.activeMarkdown.classList.add('active');
+        this.activePreview.classList.add('active');
+    };
 };
 
 module.exports = MdEditor;
