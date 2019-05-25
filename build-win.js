2d1f282109613d910af35011ef2431bb1a132a92
narugit <narusens@gmail.com>
2019-05-25 10:43:18 +0900

    Add win-builder.

---
+++ build-win.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const builder = require('electron-builder');
+const fs = require('fs');
+const packagejson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+
+builder.build({
+    platform: 'win',
+    config: {
+        'appId': `${packagejson.name}`,
+        'win': {
+            'target': {
+                'target': 'zip',
+                'arch': [
+                    'x64'
+                ]
+            },
+            'icon': 'public/icon.png'
+        }
+    },
+})
