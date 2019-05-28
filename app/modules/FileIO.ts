ac402a32607e5affecda09af60faa30aa695eeba
hiyoko3 <sb.gm.0823@gmail.com>
2019-05-28 22:09:41 +0900

    Refactor FileIO.ts

--- app/modules/FileIO.ts
+++ app/modules/FileIO.ts
@@ -6,8 +6,8 @@
 const fs = require('fs');
 const { BrowserWindow, dialog } = require('electron').remote;
 
-const FileIO = function () {
-    this.fileOpen = () => {
+class FileIO {
+    fileOpen(): void {
         const win = BrowserWindow.getFocusedWindow();
         dialog.showOpenDialog(
             win,
@@ -16,9 +16,9 @@ const FileIO = function () {
                 filters: [
                     {
                         name: 'Document',
-                        extensions: ['mmm']
-                    }
-                ]
+                        extensions: ['mmm'],
+                    },
+                ],
             },
             filePaths => {
                 if (filePaths) {
@@ -26,61 +26,72 @@ const FileIO = function () {
                 }
             }
         );
-    };
+    }
 
-    this.readFile = filePath => {
+    readFile(filePath: string): void {
         fs.readFile(filePath, (error, data) => {
             if (error !== null) {
-                alert("file open error.");
+                alert('file open error.');
                 return;
             }
 
             // parse read file.
             const json = JSON.parse(data.toString());
             this.findFields(json);
-        })
-    };
+        });
+    }
 
-    this.findFields = json => {
-        const projectPath = json.filePath;
+    findFields(json): void {
+        const projectPath: string = json.filePath;
         const _this = this;
 
-        fs.readdir(projectPath, function (err, files) {
+        fs.readdir(projectPath, (err, files) => {
             if (err) throw err;
 
             files.forEach(file => {
                 if (file.includes('memo')) {
-                    _this.embedToField(`${projectPath}/${file}`, document.querySelector('#memo-md-field'));
+                    _this.embedToField(
+                        `${projectPath}/${file}`,
+                        document.querySelector('#memo-md-field')
+                    );
                 }
 
                 if (file.includes('note')) {
-                    _this.embedToField(`${projectPath}/${file}`, document.querySelector('#note-md-field'));
+                    _this.embedToField(
+                        `${projectPath}/${file}`,
+                        document.querySelector('#note-md-field')
+                    );
                 }
 
                 if (file.includes('todo')) {
-                    _this.embedToField(`${projectPath}/${file}`, document.querySelector('#todo-md-field'));
+                    _this.embedToField(
+                        `${projectPath}/${file}`,
+                        document.querySelector('#todo-md-field')
+                    );
                 }
             });
         });
 
-        _this.addProjectNameToField(json.projectName, document.getElementById('project-name'));
-    };
+        _this.addProjectNameToField(json.projectName, document.getElementById(
+            'project-name'
+        ) as HTMLInputElement);
+    }
 
-    this.embedToField = (fileName, dom) => {
+    embedToField(fileName: string, dom: HTMLInputElement): void {
         fs.readFile(fileName, (error, data) => {
             if (error !== null) {
-                alert("file open error.");
+                alert('file open error.');
                 return;
             }
 
             // insert text.
             dom.value = data.toString();
-        })
-    };
+        });
+    }
 
-    this.addProjectNameToField = (projectName, dom) => {
+    addProjectNameToField(projectName: string, dom: HTMLInputElement): void {
         dom.dataset.projectName = projectName;
-    };
-};
+    }
+}
 
 module.exports = FileIO;
--- app/modules/MdSaver.ts
+++ app/modules/MdSaver.ts
@@ -11,26 +11,35 @@
 
 const ipc = require('electron').ipcRenderer;
 
-ipc.on('onSaveProject', (): void => {
-    const memoMdField: HTMLInputElement = document.querySelector('#memo-md-field') as HTMLInputElement;
-    const memoValue: string = memoMdField.value;
+ipc.on(
+    'onSaveProject',
+    (): void => {
+        const memoMdField: HTMLInputElement = document.querySelector(
+            '#memo-md-field'
+        ) as HTMLInputElement;
+        const memoValue: string = memoMdField.value;
 
-    const noteMdField: HTMLInputElement = document.querySelector('#note-md-field') as HTMLInputElement;
-    const noteValue: string = noteMdField.value;
+        const noteMdField: HTMLInputElement = document.querySelector(
+            '#note-md-field'
+        ) as HTMLInputElement;
+        const noteValue: string = noteMdField.value;
 
-    const todoMdField: HTMLInputElement = document.querySelector('#todo-md-field') as HTMLInputElement;
-    const todoValue: string = todoMdField.value;
+        const todoMdField: HTMLInputElement = document.querySelector(
+            '#todo-md-field'
+        ) as HTMLInputElement;
+        const todoValue: string = todoMdField.value;
 
-    const projectName:  HTMLInputElement = document.getElementById('project-name') as HTMLInputElement;
-    const name: string = projectName.dataset.projectName;
+        const projectName: HTMLInputElement = document.getElementById(
+            'project-name'
+        ) as HTMLInputElement;
+        const name: string = projectName.dataset.projectName;
 
-    const data: object = {
-        memo: memoValue,
-        note: noteValue,
-        todo: todoValue,
-        projectName: name,
-    };
-
-    ipc.send('onSendProjectData', data);
-});
-
+        const data: object = {
+            memo: memoValue,
+            note: noteValue,
+            todo: todoValue,
+            projectName: name,
+        };
+        ipc.send('onSendProjectData', data);
+    }
+);
