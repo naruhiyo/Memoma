7ebbf2706e7f2a488e8aba3b749551d5b436beee
narugit <narusens@gmail.com>
2019-06-09 14:19:12 +0900

    Add typedoc

--- src/app/modules/models/Memoma.ts
+++
@@ -1,7 +0,0 @@
-export interface Memoma {
-    memo: string;
-    note: string;
-    todo: string;
-    projectName: string;
-    projectPath: string;
-}
--- src/app/modules/models/ProjectField.ts
+++
@@ -1,4 +0,0 @@
-export interface ProjectField {
-    projectName: string;
-    filePath: string;
-}
