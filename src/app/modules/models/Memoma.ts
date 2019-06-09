b908d9881dfb9323255352bd41abe27fcb0d8c7a
hiyoko3 <sb.gm.0823@gmail.com>
2019-06-09 12:07:39 +0900

    Add typedocs to Models.

--- src/app/modules/models/Memoma.ts
+++ src/app/modules/models/Memoma.ts
@@ -1,7 +1,25 @@
+/**
+ * An entity of Memo
+ */
 export interface Memoma {
+    /**
+     * An area to summarize the anything.
+     */
     memo: string;
+    /**
+     * An area which can write what you think.
+     */
     note: string;
+    /**
+     * A `Todo` list that you need.
+     */
     todo: string;
+    /**
+     * A project name.
+     */
     projectName: string;
+    /**
+     * A path of placed the project.
+     */
     projectPath: string;
 }
--- src/app/modules/models/ProjectField.ts
+++ src/app/modules/models/ProjectField.ts
@@ -1,4 +1,14 @@
+/**
+ * An entity of Project
+ */
 export interface ProjectField {
+    /**
+     * A project name.
+     */
     projectName: string;
+    /**
+     * A path of placed the project.
+     * This is similar with `[Memoma projectPath]()`.
+     */
     filePath: string;
 }
