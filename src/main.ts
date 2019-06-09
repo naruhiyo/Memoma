ff80f5c95ed1b00fb2ba0ee3f5464110f5e91221
narugit <narusens@gmail.com>
2019-06-09 13:45:11 +0900

    Add typedoc to public ts files and main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,33 @@
-'use strict';
+/**
+ * ## Description
+ * Main process.
+ * 
+ * ## Functions
+ * - Open window.
+ * - Close window.
+ * - Manage menu controll.
+ */
 
-// global module
+/**
+ * Import modules
+ */
 import { app, BrowserWindow, Menu, shell, ipcMain, dialog } from 'electron';
 import { ProjectManager } from './app/modules/ProjectManager.js';
 import { Memoma } from "./app/modules/models/Memoma";
 
+/**
+ * Create and save project
+ */
 const projectManager = new ProjectManager();
 
-// メインウィンドウはGCされないようにグローバル宣言
+/**
+ * Create and control browser windows
+ */
 let mainWindow: Electron.BrowserWindow | null;
 
-// Executed after the electron started.
 app.on('ready', () => {
     let projectPath: string | undefined;
 
-    // Main process
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -22,12 +35,13 @@ app.on('ready', () => {
             nodeIntegration: true,
         },
     });
+
     mainWindow.loadURL(`file://${__dirname}/index.html`);
     mainWindow.webContents.on('new-window', (event, url) => {
         event.preventDefault();
         shell.openExternal(url);
     });
-    // App exit.
+
     mainWindow.on('closed', () => {
         mainWindow = null;
         if (process.platform !== 'darwin') {
@@ -35,7 +49,6 @@ app.on('ready', () => {
         }
     });
 
-    // The global menu of Electron app.
     const templateMenu: Electron.MenuItemConstructorOptions[] = [
         {
             label: 'File',
--- src/public/modules/projectNameCreator.ts
+++ src/public/modules/projectNameCreator.ts
@@ -1,7 +1,27 @@
-'use strict';
+/**
+ * ## Description
+ * Client Side Modules.
+ * Create project name.
+ *
+ * ## Functions
+ * - Toggle project-save form whne receiving `onProjectNameInFill` event from `main.ts` or clicking save button.
+ * - Get project name from text box when clicking save button.
+ * - Send project name to `main.ts`.
+ */
 
+/**
+ * Communicate asynchronously from a renderer process in projectNameCreator to the main process
+ */
 const pncIpcRenderer: Electron.IpcRenderer = require('electron').ipcRenderer;
+
+/**
+ * Save button
+ */
 const saveBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById('save_btn');
+
+/**
+ * Toggle elements for project saving
+ */
 const projectSaver: HTMLElement = <HTMLElement>document.getElementById('project-saver');
 
 if (saveBtn !== null) {
